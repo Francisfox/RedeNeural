d4ffad68847b8ef417198910e973778ce7ed2540
Francisfox <francismarfox@hotmail.com>
2021-07-14 17:14:58 +0000

    Rede

--- sketch.js
+++ sketch.js
@@ -1,4 +1,8 @@
 var train = true;
+var input1 = 0;
+var input2 = 1;
+var input3 = 2;
+var input4 = 3;
 
 function setup() {
     createCanvas(500, 500);
@@ -10,25 +14,32 @@ function setup() {
     dataset = {
         inputs:
             [[1, 1],
-            [1, 0],
-            [0, 1],
-            [0, 0]],
+             [1, 0],
+             [0, 1],
+             [0, 0]
+            ],
         outputs:
-            [[0],
-            [1],
-            [1],
-            [0]]
+            [[1],
+             [0],
+             [1],
+             [0]
+            ]
     }
 }
 
 function draw() {
     if (train) {
-        for (var i = 0; i < 10000; i++) {
+        for (var i = 0; i < 1000; i++) {
             var index = floor(random(4));
             nn.train(dataset.inputs[index], dataset.outputs[index]);
         }
-        if (nn.predict([0, 0])[0] < 0.04 && nn.predict([1, 0])[0] > 0.98) {
+        if (nn.predict(dataset.inputs[input1]) > 0.98 && nn.predict(dataset.inputs[input4]) < 0.04) {
             train = false;
+            console.log(nn.predict(dataset.inputs[input1]));
+            console.log(nn.predict(dataset.inputs[input2]));
+            console.log(nn.predict(dataset.inputs[input3]));
+            console.log(nn.predict(dataset.inputs[input4]));
+        
             console.log("terminou");
         }
     }
